e021484e2d2a50e1ac3c0067ee301132919eb0fc
Suzanne Loures <suzanneloures@gmail.com>
2020-11-22 21:21:46 -0300

    criando tabela employees com make:migration

---
+++ src/employee_api_rest/database/migrations/2020_11_22_235145_create_table_employees.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableEmployees extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->date('admission');
+            $table->float('salary');
+            $table->timestamps(); //created_at e update_at
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
