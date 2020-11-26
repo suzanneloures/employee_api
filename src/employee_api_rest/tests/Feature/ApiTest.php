2571827dfae8f018ae192ba1904601feabbfd2e8
Suzanne Loures <suzanneloures@gmail.com>
2020-11-26 15:54:44 -0300

    criando ApiTest para get e post e adicionando pagina de web_app utilizando api

---
+++ src/employee_api_rest/tests/Feature/ApiTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ApiTest extends TestCase
+{
+
+    public function test_get_json_employee_return_sucess()
+    {
+        $response = $this->getJson('/api/employees');
+        $response->assertStatus(200);
+    }
+
+    public function test_post_json_employee_return_create()
+    {
+        $response = $this->postJson('/api/employees', ['name' => 'Funcionario', 'email'=>'funciorioteste@hotmail.com', "admission" => '2020-01-01', 'salary' => '1500']);
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'status' => 201,
+                ]
+            ]);
+    }
+}
